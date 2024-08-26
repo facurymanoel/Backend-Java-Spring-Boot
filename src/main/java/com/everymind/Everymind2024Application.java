626fd030088366adcda24a1d8ca3527787dab60f
dalmo <facurymanoel10@gmail.com>
2024-08-26 13:45:10 -0300

    projeto iniciado, configuração do application.properties

---
+++ src/main/java/com/everymind/Everymind2024Application.java
@@ -0,0 +1,13 @@
+package com.everymind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Everymind2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Everymind2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/everymind/Everymind2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everymind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Everymind2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
