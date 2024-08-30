c82c992c04688437b7dcc4dd6037b137dd45f66b
dalmo <facurymanoel10@gmail.com>
2024-08-30 12:30:02 -0300

    Acrescentando a anotação @CrossOrigin na Classe ProdutoController

--- src/main/java/com/everymind/controller/ProdutoController.java
+++ src/main/java/com/everymind/controller/ProdutoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import com.everymind.service.ProdutoService;
 
 import lombok.AllArgsConstructor;
 
+@CrossOrigin(origins = "*")
 @AllArgsConstructor
 @RestController
 @RequestMapping("/produtos")
